fa00a51bd6c8390a9a5f07eac25caa60e914a25a
pavel_flyagin <pavel_flyagin@icloud.com>
2017-12-04 16:02:07 +0500

    initial commit

---
+++ cs/Testing/Examples.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Testing
+{
+    internal class Calculator
+    {
+        private int currentSum;
+
+        public double Result()
+        {
+            return currentSum;
+        }
+
+        public void Add(int number)
+        {
+            checked
+            {
+                currentSum += number;
+            }
+        }
+    }
+
+    [TestFixture]
+    internal class CalculatorSpec
+    {
+        [TestFixture]
+        public class Sum
+        {
+            [Test]
+            public void Should_return_0_by_default()
+            {
+                var calc = new Calculator();
+
+                var sum = calc.Result();
+
+                Assert.AreEqual(0, sum);
+            }
+
+            [Test]
+            public void Should_be_0_by_default_with_fluentAssertions()
+            {
+                var calc = new Calculator();
+
+                var sum = calc.Result();
+
+                sum.Should().Be(10);
+            }
+        }
+
+        [TestFixture]
+        public class Add
+        {
+            [TestCase(new[] { 1, 2 }, 3, TestName = "1 + 2 = 3")]
+            [TestCase(new[] { 5, 10 }, 15, TestName = "5 + 10 = 15")]
+            [TestCase(new[] { 1, 2, 3, 4 }, 10, TestName = "1 + 2 + 3 + 4 = 10")]
+            public void Should_add_value_to_sum(int[] numbers, int expectedResult)
+            {
+                var calc = new Calculator();
+
+                foreach (var number in numbers)
+                    calc.Add(number);
+
+                var sum = calc.Result();
+                Assert.AreEqual(expectedResult, sum);
+            }
+
+            [Test]
+            public void Should_fail_if_accumulated_value_overflow()
+            {
+                var calc = new Calculator();
+                calc.Add(int.MaxValue);
+
+                TestDelegate act = () => calc.Add(1);
+
+                Assert.Throws<OverflowException>(act);
+            }
+
+            [Test]
+            public void Should_fail_if_accumulated_value_overflow_with_fluent_assertions()
+            {
+                var calc = new Calculator();
+                calc.Add(int.MaxValue);
+
+                Action act = () => calc.Add(1);
+
+                act.ShouldThrow<OverflowException>();
+            }
+        }
+    }
+
+    [TestFixture]
+    internal class NUnitLifeCycleTests
+    {
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+            Console.WriteLine("OneTimeSetUp");
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            Console.WriteLine("SetUp");
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            Console.WriteLine("TearDown");
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            Console.WriteLine("OneTimeTearDown");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("test1");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Console.WriteLine("test2");
+        }
+    }
+}
---
+++ js/tests/Examples.js
@@ -0,0 +1,108 @@
+import assert from 'assert';
+
+class Calculator {
+    constructor() {
+        this._currentSum = 0;
+    }
+
+    add(number) {
+        if (isNaN(number))
+            throw Error;
+        this._currentSum += number;
+    }
+
+    sum() {
+        return this._currentSum;
+    }
+}
+
+it("test", () => {
+    const calc = new Calculator();
+    calc.add(2);
+    calc.add(2);
+    const sum = calc.sum();
+    assert.equal(4, sum);
+});
+
+describe("Calculator specification", () => {
+
+    describe("add", () => {
+
+        [
+            {numbers: [1, 2], result: 3},
+            {numbers: [10, 5], result: 15},
+            {numbers: [1, 2, 3, 4], result: 10},
+        ].forEach((x) => {
+            it(`should add given number to accumulated value. ${x.numbers.join('+')}=${x.result}`, () => {
+                const calculator = new Calculator();
+
+                for (let i = 0; i < x.numbers.length; i++) {
+                    calculator.add(x.numbers[i]);
+                }
+
+                const sum = calculator.sum();
+                assert.equal(x.result, sum);
+            });
+        });
+
+        it("should fail if NaN parameter passed", () => {
+            const calc = new Calculator();
+
+            const act = () => calc.add(NaN);
+
+            assert.throws(act);
+        });
+
+        it("should fail if NaN parameter passed with chai", () => {
+            const calc = new Calculator();
+
+            const act = () => calc.add(NaN);
+
+            act.should.throw();
+        });
+    });
+
+    describe("sum", () => {
+
+        it("should return zero by default", () => {
+            const calc = new Calculator();
+
+            const result = calc.sum();
+
+            assert.equal(0, result)
+        });
+
+        it("should return zero by default with fluent assertions", () => {
+            const calc = new Calculator();
+
+            const result = calc.sum();
+
+            result.should.be.equal(0);
+        });
+    })
+});
+
+
+describe("mocha life cycle tests", () => {
+    before(() => {
+        console.log("before all");
+    });
+    beforeEach(() => {
+        console.log("before each");
+    });
+    afterEach(() => {
+        console.log("after each");
+    });
+    after(() => {
+        console.log("after all")
+    });
+    it("test1", () => {
+        console.log("test1")
+    });
+    it("test2", () => {
+        console.log("test2");
+    });
+});
+
+
+
